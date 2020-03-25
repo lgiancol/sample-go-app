412ff1a7671a79c2362a0e8c7b187946314d0a5d
Luu <lucas.giancola@live.com>
2020-03-25 12:51:49 -0400

    Removed habitat directory for SHO testing

--- habitat/plan.sh
+++
@@ -1,7 +0,0 @@
-pkg_name=sample-go-app
-pkg_origin=shuttleops
-pkg_version="0.1.0"
-pkg_scaffolding="core/scaffolding-go"
-pkg_svc_user="root"
-pkg_bin_dirs=(bin)
-scaffolding_go_base_path="github.com/lgiancol"
